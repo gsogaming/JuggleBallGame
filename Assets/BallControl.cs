3385aea29bcb0899ea5668369d422012cb513af8
gsogaming <gokselserkanoguz@gmail.com>
2023-10-25 03:09:41 +0300

    InitialSteps

--- Assets/BallControl.cs
+++ Assets/BallControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FeetController : MonoBehaviour
+public class BallControl : MonoBehaviour
 {
     Rigidbody ballRb;
     
--- Assets/FeetController.cs
+++ Assets/FeetController.cs
@@ -2,67 +2,112 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseFollow : MonoBehaviour
-{
-    [SerializeField] float moveSpeed;
-    private Rigidbody rb;
+public class FeetController : MonoBehaviour
+{    
+    
+    public GameObject rightFoot, leftFoot;
+    public GameObject orientationObject;
     private Vector3 targetPosition;
-    private Vector3 originalPos;
-    [SerializeField] bool rightFoot;
+    private Vector3 originalPosRight;
+    private Vector3 originalRotRight;
+    private Vector3 originalPosLeft;
+    private Vector3 originalRotLeft;
+    private bool rightFootSelected;
+    private bool leftFootSelected;
+    
+
+  
+
+
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
         
-        originalPos = transform.position;
-        if (this.gameObject.tag == "RightFoot")
-        {
-            rightFoot = true;
-        }
-        else
-        {
-            rightFoot = false;
-        }
-        
+        originalPosRight = rightFoot.transform.position;
+        originalRotRight = rightFoot.transform.rotation.eulerAngles;
+        originalPosLeft = leftFoot.transform.position;
+        originalRotLeft = leftFoot.transform.rotation.eulerAngles;
+
+              
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0) && rightFoot)
-        {            
-            
-            // Get the mouse position in screen coordinates
-            Vector3 mousePos = Input.mousePosition;
-
-            // Set the Z position to a constant value to ensure the object doesn't get too close or too far
-            mousePos.z = 10;
-
-            // Convert the screen coordinates to world coordinates
-            targetPosition = Camera.main.ScreenToWorldPoint(mousePos);
-        }
-        else if(Input.GetMouseButton(1) && !rightFoot)
-        {
-            // Get the mouse position in screen coordinates
-            Vector3 mousePos = Input.mousePosition;
-
-            // Set the Z position to a constant value to ensure the object doesn't get too close or too far
-            mousePos.z = 10;
-
-            // Convert the screen coordinates to world coordinates
-            targetPosition = Camera.main.ScreenToWorldPoint(mousePos);
-        }
-        else
-        {
-            transform.position = originalPos;
-        }
+        ButtonControls();
         
+
+
     }
-        
+
+    private void ButtonControls()
+    {
+        if (Input.GetMouseButton(0))
+        {
+
+            // Get the mouse position in screen coordinates
+            Vector3 mousePos = Input.mousePosition;
+
+            // Set the Z position to a constant value to ensure the object doesn't get too close or too far
+            mousePos.z = 10;
+
+            // Convert the screen coordinates to world coordinates
+            targetPosition = Camera.main.ScreenToWorldPoint(mousePos);
+            rightFootSelected = false;
+            leftFootSelected = true;
+
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            // Get the mouse position in screen coordinates
+            Vector3 mousePos = Input.mousePosition;
+
+            // Set the Z position to a constant value to ensure the object doesn't get too close or too far
+            mousePos.z = 10;
+
+            // Convert the screen coordinates to world coordinates
+            targetPosition = Camera.main.ScreenToWorldPoint(mousePos);
+            rightFootSelected = true;
+            leftFootSelected = false;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            
+            leftFootSelected = false;
+            leftFoot.transform.position = originalPosLeft;
+            leftFoot.transform.rotation = Quaternion.Euler(originalRotLeft);
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            
+            rightFootSelected = false;
+            rightFoot.transform.position = originalPosRight;
+            rightFoot.transform.rotation = Quaternion.Euler(originalRotRight);
+        }
+
+    }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(targetPosition);
+        if (rightFootSelected)
+        {
+            rightFoot.GetComponent<Rigidbody>().MovePosition(targetPosition);
+            rightFoot.transform.LookAt(orientationObject.transform.position);
+            rightFoot.GetComponent<Collider>().enabled = true;
+            leftFoot.GetComponent<Collider>().enabled = false;
+
+
+        }
+        else if (leftFootSelected)
+        {
+
+            leftFoot.GetComponent<Rigidbody>().MovePosition(targetPosition);
+            leftFoot.transform.LookAt(orientationObject.transform.position);
+            leftFoot.GetComponent<Collider>().enabled = true; ;
+            rightFoot.GetComponent<Collider>().enabled = false;
+        }
+       
+
     }
 }
